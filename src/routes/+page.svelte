ef4907d7dafb643369a83d37fad7048dfe4534a5
Jin G <gao-jin@outlook.com>
2024-09-06 06:51:49 -0400

    Lab 4 4 templating from json file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 <title>Home</title>
 </svelte:head>
 
+
+
 <h1> Jin Gao </h1>
 <p> Jin is a passionate designer and tech lover! </p>
 <img src="images/tim.jpg" alt="Tim's photo" width="200" height="200">
+
