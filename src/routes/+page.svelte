97e92818f5a9bd6cb81fbdb3b149c6dca38cfd51
Jin G <gaojin@mit.edu>
2024-09-16 10:51:48 -0400

    updating front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-  import projects from "$lib/projects.json";
-  import Project from "$lib/Project.svelte";
+  import projects from "$lib/dummyprojects.json";
+  import HeaderProject from "$lib/HeaderProject.svelte";
 </script>
 
 <svelte:head>
@@ -8,18 +8,20 @@
 </svelte:head>
 
 <h1 class="big-title">Welcome to Jin's Visualization Treasure Box</h1>
+
+
 <img src="images/tim.jpg" alt="Tim's photo" width="200" height="200" />
 
 <div class="projects">
   {#each projects.slice(0, 3) as project}
-    <Project data={project} hLevel="3" />
+    <HeaderProject data={project} hLevel="3" />
   {/each}
 </div>
 
 <style>
     .projects {
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(15em, 1fr)); /* Ensures responsive columns */
+        grid-template-columns: 1fr; /* Ensures responsive columns */
         gap: 1em; /* Space between grid items */
         margin-top: 1em;
     }
--- src/routes/labs/lab6_project_grids/+page.svelte
+++ src/routes/labs/lab6_project_grids/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import projects from "$lib/projects.json";
+  import projects from "$lib/dummyprojects.json";
   import Project from "$lib/Project.svelte";
   import Pie from "$lib/Pie.svelte";
   import * as d3 from "d3";
