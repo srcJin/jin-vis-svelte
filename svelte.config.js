bfee872229893caf325fca320f011a7e2dd8cd09
Jin G <gaojin@mit.edu>
2024-09-17 15:48:29 -0400

    updating svelte base path

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			// set base path
+			base: process.env.NODE_ENV === 'production' ? '/jin-vis-svelte' : '',
 		},
 	},
 };
